c35c53c226eef53cc45c1248ea1040162730a41a
solangealmn <solangealmn@gmail.com>
2018-09-17 16:59:50 -0300

    exercicio 2 resolvido

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-const btnBusca = document.getElementById("btn-busca");
-btnBusca.addEventListener("click", trazBusca);
+$(document).ready(() => {
+  const btnBusca = document.getElementById("btn-busca");
+  btnBusca.addEventListener("click", trazBusca);
+
+});
+
 let docs = [];
 
 function buscaPalavra(){
@@ -12,14 +16,18 @@ function erro(){
 
 function trazBusca(event){
   event.preventDefault();
-  const pegarNotícia = new XMLHttpRequest();
-  pegarNotícia.open("GET",  `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${buscaPalavra()}&page=2&sort=oldest&api-key=a0bb9d25234c494086ca1611e7395010`);
-  pegarNotícia.onload = carregarPosts;
-  pegarNotícia.onerror = erro;
-  pegarNotícia.send();
+  const url = `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${buscaPalavra()}&page=2&sort=oldest&api-key=a0bb9d25234c494086ca1611e7395010`;
+  $.ajax({
+    type:"GET",
+    url,
+    success: carregarPosts,
+    error: erro,
+    crossDomain: true
+  });
 }
-function carregarPosts(){
-  docs = JSON.parse(this.responseText)["response"]["docs"];
+function carregarPosts(data){
+  docs = data.response.docs;
+  // docs = JSON.parse(this.responseText)["response"]["docs"];
   exibePosts();
 }
 
@@ -28,7 +36,7 @@ function exibePosts(){
   exibeBusca.innerHTML = `
   <div class="area-noticia">${docs.map(doc => `
     <div class="noticia">
-    <a href="${docs.web_url}">
+    <a href="${doc.web_url}">
     <h3>${doc.headline.main}</h3>
     <p>${doc.snippet}</p></a>
   </div>`).join("")}</div>`;
